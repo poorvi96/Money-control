1ce79510064a4e84bb045a5b80d5df6680548f39
hanuman12511 <er.hanumansaini@gmail.com>
2023-08-26 13:51:51 +0530

    fix

--- src/routes/Router.js
+++ src/routes/Router.js
@@ -1,11 +1,11 @@
 import React from 'react'
 import { Text } from 'react-native'
-import HomeScreen from '../screens/Homescreen'
+import Homescreen from '../screens/Homescreen'
 import { NavigationContainer } from '@react-navigation/native';
 export default function Router() {
   return (
     <NavigationContainer>
-      <HomeScreen />
+      <Homescreen />
     </NavigationContainer>
 
 
