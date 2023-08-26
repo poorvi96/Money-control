1cb732cc9e010848c4b7a86a04f1c4053f4c5d0a
hanuman12511 <er.hanumansaini@gmail.com>
2023-08-26 13:56:53 +0530

    sdfsdfd

--- src/screens/Homescreen.js
+++ src/screens/Homescreen.js
@@ -3,7 +3,7 @@ import React from 'react'
 import Header from '../components/Header'
 import TopTabBar from '../components/TopTabBar';
 
-export default function HomeScreen() {
+export default function Homescreen() {
     return (
         <>
             
