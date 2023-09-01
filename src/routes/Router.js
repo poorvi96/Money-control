c9da3527c52383d2827a7d27944ebd683607e05e
poorvi <mittalpoorvi123@gmail.com>
2023-09-01 12:02:26 +0530

    nbjbxsj

--- src/routes/Router.js
+++ src/routes/Router.js
@@ -7,15 +7,20 @@ import { createStackNavigator } from '@react-navigation/stack';
 import AmountCard from '../screens/AmountCard';
 import ShowCard from '../screens/ShowCard';
 import ShowData from '../screens/ShowData';
+
 const Stack = createStackNavigator();
 export default function Router() {
   return (
     <NavigationContainer>
     <Stack.Navigator screenOptions={{headerShown:false}}>
+     
         <Stack.Screen name="homescreen" component={Homescreen}/>
         <Stack.Screen name="amountcard" component={AmountCard}/>
         <Stack.Screen name="showcard" component={ShowCard}/>
         <Stack.Screen name="showdata" component={ShowData}/>
+       
+
+        
 
 
         </Stack.Navigator>
